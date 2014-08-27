1a3e00f2462bf53bd77afde6982a0179e91bda2b
Felix Rieseberg <felix@felixrieseberg.com>
2014-08-27 12:03:13 -0700

    Initial Commit

---
+++ index.js
@@ -0,0 +1,9 @@
+var downsize = require('downsize'),
+	console = require('better-console');
+
+console.clear();
+
+// Commencing test
+
+console.info('Testing Downsize 0.0.5 for various words=0 cases \n' + 
+			 '------------------------------------------------');
